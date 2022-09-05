7246f14728d9d68d67b40d06622f9c1922d7a4f4
Ирина Никишина <lilas_pourpre@Irinas-MacBook-Pro.local>
2022-09-05 23:50:39 +0200

    update models

--- api/src/load_bert.py
+++ api/src/load_bert.py
@@ -1,9 +1,9 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='15WsRd6Bh8TAuEnkCgadDvOm5eyuT8lxJ',
+gdd.download_file_from_google_drive(file_id='1OiZOaA2pKTmyJbDpQPds3QYeBhZw6fC-',
                                     dest_path='./src/projection_model_gb-b.pt',
                                     unzip=False)
 
-gdd.download_file_from_google_drive(file_id='1PKzG2OsIsQICM2vare5k6_TplMhjs1eZ',
+gdd.download_file_from_google_drive(file_id='1hM3byvh3iDRWAzZ6mmAt3-C5U1gZfRmy',
                                     dest_path='./src/node_graph_reconstruct_model_directed.txt',
                                     unzip=False)
