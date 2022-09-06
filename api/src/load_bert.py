c530c254ba09a0f291ac4ac1cf08b49970f62745
Ирина Никишина <lilas_pourpre@dhcp-172-21-36-208.wlan.uni-hamburg.de.local>
2022-09-06 11:32:20 +0200

    update models

--- api/src/load_bert.py
+++ api/src/load_bert.py
@@ -1,4 +1,3 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
 import gdown
 
 url = 'https://drive.google.com/uc?id=1hM3byvh3iDRWAzZ6mmAt3-C5U1gZfRmy'
@@ -6,8 +5,9 @@ output = './src/node_graph_reconstruct_model_directed.txt'
 gdown.download(url, output, quiet=False)
 
 
-gdd.download_file_from_google_drive(file_id='1OiZOaA2pKTmyJbDpQPds3QYeBhZw6fC-',
-                                    dest_path='./src/projection_model_gb-b.pt',
-                                    unzip=False)
+url = 'https://drive.google.com/uc?id=1OiZOaA2pKTmyJbDpQPds3QYeBhZw6fC-'
+output = './src/projection_model_gb-b.pt'
+gdown.download(url, output, quiet=False)
+
 
 
