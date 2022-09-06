8b40b98a31ed9e2a46b4fc7379ecb160462411a7
Ирина Никишина <lilas_pourpre@dhcp-172-21-36-208.wlan.uni-hamburg.de.local>
2022-09-06 10:46:12 +0200

    new way of model download

--- api/src/load_bert.py
+++ api/src/load_bert.py
@@ -1,9 +1,13 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
+import gdown
+
+url = 'https://drive.google.com/uc?id=1hM3byvh3iDRWAzZ6mmAt3-C5U1gZfRmy'
+output = './src/node_graph_reconstruct_model_directed.txt'
+gdown.download(url, output, quiet=False)
+
 
 gdd.download_file_from_google_drive(file_id='1OiZOaA2pKTmyJbDpQPds3QYeBhZw6fC-',
                                     dest_path='./src/projection_model_gb-b.pt',
                                     unzip=False)
 
-gdd.download_file_from_google_drive(file_id='1hM3byvh3iDRWAzZ6mmAt3-C5U1gZfRmy',
-                                    dest_path='./src/node_graph_reconstruct_model_directed.txt',
-                                    unzip=False)
+
