97e252bdc6badc34ab4b6b58ad3b7687647f0ef6
Irina Nikishina <irina,nikishina@mail.ru>
2024-11-28 13:40:57 +0100

    add adaptation for both servers

--- api/src/helpers.py
+++ api/src/helpers.py
@@ -1,8 +1,10 @@
 import uuid
+import torch
 from collections import defaultdict
 from nltk.corpus import wordnet as wn
 from nltk.corpus.reader.wordnet import WordNetError
 from taxollama import predict_node_from_taxollama
+from gbert import predict_node_from_bert
 
 
 def get_graph_with_node(start_node):
@@ -71,7 +73,10 @@ def _get_relations(all_nodes):
 
 def generate_new_node(graph, start_node, candidates, cur_index, end_node=None):
     level, start_name = get_level_and_start_name(graph, start_node)
-    node_x = predict_node_from_taxollama(start_name, candidates, cur_index, end_node)
+    if torch.cuda.is_available():
+        node_x = predict_node_from_taxollama(start_name, candidates, cur_index, end_node)
+    else:
+        node_x = predict_node_from_bert(start_name, candidates, cur_index)
     new_word = {
             'id': str(uuid.uuid4()),
             'word': node_x,
