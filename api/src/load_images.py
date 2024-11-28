3019b7c7e5b462346b3be0992db8fda55ddd88ae
Irina Nikishina <irina,nikishina@mail.ru>
2024-11-28 12:11:00 +0100

    update images

--- api/src/load_images.py
+++ api/src/load_images.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1T9iiNkp2X8n-W-N2MRZ6waQ0OkUulDzy'
+url = 'https://drive.google.com/uc?id=1VNWluYhO5jOxu6qO_FhJKR7a2NkMFOdJ'
 output = 'src/wn_images.zip'
 gdown.download(url, output, quiet=False)
