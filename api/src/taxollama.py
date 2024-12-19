45b471c1b425f313648d34f16b6ed381f74d5842
Irina Nikishina <irina.nikishina@mail.ru>
2024-12-19 22:29:16 +0100

    update model to bfloat16

--- api/src/taxollama.py
+++ api/src/taxollama.py
@@ -3,7 +3,7 @@ import os
 import torch
 from dotenv import load_dotenv
 from peft import PeftConfig, PeftModel
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from prompting import SYSTEM_PROMPT
 
@@ -12,17 +12,16 @@ load_dotenv()
 def load_model_and_tokenizer():
     if torch.cuda.is_available():
         torch.set_default_device(f"cuda:0")
-    config = PeftConfig.from_pretrained("VityaVitalich/TaxoLLaMA_All")
-    model = LlamaForCausalLM.from_pretrained(
-        config.base_model_name_or_path,
-        torch_dtype=torch.bfloat16,
-        token=os.getenv("HF_TOKEN"),
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        'VityaVitalich/TaxoLlama3.1-8b-instruct',
+        torch_dtype=torch.bfloat16
     )
-    tokenizer = LlamaTokenizer.from_pretrained(
-        config.base_model_name_or_path, token=os.getenv("HF_TOKEN")
+    tokenizer = AutoTokenizer.from_pretrained(
+        'meta-llama/Llama-3.1-8B', token=os.getenv("HF_TOKEN")
     )
-    inference_model = PeftModel.from_pretrained(model, "VityaVitalich/TaxoLLaMA_All")
-    return inference_model, tokenizer
+    
+    return model, tokenizer
 
 
 inference_model, tokenizer = load_model_and_tokenizer()
@@ -42,7 +41,6 @@ def generate_candidates(word, last_word):
     processed_term += " | synset:"
 
     processed_term = SYSTEM_PROMPT + "\n" + processed_term + "[/INST]"
-
     input_ids = tokenizer(processed_term, return_tensors="pt")
 
     gen_conf = {
@@ -55,8 +53,9 @@ def generate_candidates(word, last_word):
     }
 
     out = inference_model.generate(inputs=input_ids["input_ids"].to("cuda"), **gen_conf)
-
+    print(tokenizer.batch_decode(out)[0])
     text = tokenizer.batch_decode(out)[0][len(SYSTEM_PROMPT) :].split("[/INST]")[-1]
+    print(text)
     return text.split(",")
 
 
