be95b1e4eb1c87f6a8f316bd4755e6b5889b59a5
Irina Nikishina <irina.nikishina@mail.ru>
2025-03-06 15:38:45 +0100

    add new images

--- api/src/load_images.py
+++ api/src/load_images.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1VNWluYhO5jOxu6qO_FhJKR7a2NkMFOdJ'
-output = 'src/wn_images.zip'
+url = 'https://drive.google.com/uc?id=1T9iiNkp2X8n-W-N2MRZ6waQ0OkUulDzy' #1VNWluYhO5jOxu6qO_FhJKR7a2NkMFOdJ'
+output = 'wn_images2.zip'
 gdown.download(url, output, quiet=False)
